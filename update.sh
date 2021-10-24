3a796fcfaae01360a6e2b7d8dee5c6fad0988105
Nick <nickmok@gmail.com>
2021-10-23 19:42:36 -0700

    yes

---
+++ update.sh
@@ -0,0 +1,9 @@
+cd /home/nicholas/Downloads
+sudo rm -rf flipClock
+
+git clone git@github.com:nicknamemok/flipClock.git
+
+cd /home/nicholas/RPi
+sudo rm -rf flipClock
+
+sudo mv /home/nicholas/Downloads/flipClock .
