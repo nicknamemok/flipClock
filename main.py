750bb78bb7343ed1687aaab22ea9c9feb6b1167b
Nick <nickmok@gmail.com>
2022-01-29 00:57:20 -0800

    edits to make it work in testing

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ NUMBER_OF_TEETH = 2048
 GEAR_RATIO = 3
 NUMBER_OF_FLAPS = 12
 DELAY_FLIP = 0.5
-DELAY_STEP = 0.0025
+DELAY_STEP = 0.0005
 SERVO_PINS = [[1,2,3,4],        # Hour, digit 1
               [1,2,3,4],        # Hour, digit 2
               [12,16,20,21],    # Minute, digit 1
@@ -51,12 +51,15 @@ class Servo:
     def stepServo(self) -> None:
         # Output array to pins
         for pin in range(len(self.m_pins)):
-            GPIO.output(self.m_pins[pin],outputs[pin])
+            GPIO.output(self.m_pins[pin],self.m_stepOutput[self.m_stepOutputIndex][pin])
         # Increment stepoutput index cyclically
         if self.m_stepOutputIndex == len(self.m_stepOutput)-1:
             self.m_stepOutputIndex = 0 
         else:
             self.m_stepOutputIndex += 1
+
+    def setPosition(self, position: int) -> None:
+        self.currentPosition = position
 
 
 class Controller:
@@ -72,10 +75,10 @@ class Controller:
         # GPIO setup
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        self.servos = [Servo() for pins in SERVO_PINS]
+        self.servos = [Servo(pins) for pins in SERVO_PINS]
 
     # Move to input position
-    def moveTo(command: List[int]) -> None:
+    def moveTo(self, command: List[int]) -> None:
         # Calculate steps needed for move
         movement = [0]*len(self.servos)
         for i in range(len(self.servos)):
@@ -94,9 +97,10 @@ class Controller:
                     self.servos[i].stepServo()
                     movement[i] -= 1
                     time.sleep(self.stepDelay)
+        {self.servos[i].setPosition(command[i]) for i in range(len(self.servos))}
 
     # Move single servo 1 flap
-    def moveSingle(servo: int) -> None:
+    def moveSingle(self, servo: int) -> None:
         if self.servos[i].currentPosition < self.numberOfFlaps:
             {self.servos[i].stepServo() for i in range(self.normalSteps)}
             self.servos[i] += 1
@@ -105,16 +109,17 @@ class Controller:
             self.servos[i] = 0
 
     # Unit test to time 1111
-    def unitTest():
+    def unitTest(self):
         self.moveTo([1,1,1,1])
         {print(servo.currentPosition) for servo in self.servos}
 
     # Unit test cycle all flaps
-    def cycleTest():
+    def cycleTest(self):
         for i in range(self.numberOfFlaps):
             self.moveTo([i]*4)
             {print(servo.currentPosition) for servo in self.servos}
             time.sleep(self.flipDelay)
+        self.moveTo([0]*4)
 
             
 
@@ -129,7 +134,7 @@ def main():
     for opt, arg in optlist:
         # Single flip test
         if opt in ("-s"):
-            print("Single step test: ",arg)
+            print("Single step test")
             controller.unitTest()
             return
         # Cycle flip test
